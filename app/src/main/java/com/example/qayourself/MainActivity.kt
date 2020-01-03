3dc67db007ab06ce0f1ebf8459066646372e69f9
juckrit <juckrit072@gmail.com>
2020-01-03 09:47:43 +0700

    Initial commit

---
+++ app/src/main/java/com/example/qayourself/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.qayourself
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.qayourself.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
