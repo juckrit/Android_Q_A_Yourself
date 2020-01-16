102e24d4325819fb4b31eada31fc285f3d603bec
juckrit072@gmail.com <juckrit072@gmail.com>
2020-01-16 15:10:10 +0700

    filter version1

--- app/src/main/java/com/example/qayourself/adapter/PlayAllQuestionGroupAdapter.kt
+++ app/src/main/java/com/example/qayourself/adapter/PlayAllQuestionGroupAdapter.kt
@@ -4,17 +4,56 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Filterable
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.example.qayourself.R
 import com.example.qayourself.Room.Question
 import com.example.qayourself.ui.Fragment.UI_Play_Page.PlayingAllQuestionFragmentDirections
 import kotlinx.android.synthetic.main.list_of_question.view.*
+import java.util.logging.Filter
+import java.util.logging.LogRecord
 
 class PlayAllQuestionGroupAdapter(val context: Context, val dataList: MutableList<Question>) :
-    RecyclerView.Adapter<PlayAllQuestionGroupAdapter.ViewHolder>() {
+    RecyclerView.Adapter<PlayAllQuestionGroupAdapter.ViewHolder>()
+    , Filterable {
 
+
+
+
+    private var myFilter: android.widget.Filter = object : android.widget.Filter() {
+        override fun performFiltering(constraint: CharSequence?): FilterResults {
+            var filteredList = mutableListOf<Question>()
+            if (constraint==null || constraint.isEmpty()) {
+                filteredList.addAll(dataList)
+            } else {
+                var filterPattern = constraint.toString().toLowerCase().trim()
+                dataList.forEach {
+                    if (it.questionTitle.equals(filterPattern, true)) {
+                        filteredList.add(it)
+                    }
+                }
+            }
+            var result = FilterResults()
+            result.values = filteredList
+            return result
+        }
+
+        override fun publishResults(constraint: CharSequence?, results: FilterResults?) {
+            dataList.clear()
+            dataList.addAll(results?.values as Collection<Question>)
+            notifyDataSetChanged()
+        }
+    }
     private val inflater = LayoutInflater.from(context)
+
+
+
+
+    override fun getFilter(): android.widget.Filter {
+        return myFilter
+    }
+
 
     fun updateQuestions(questions: List<Question>) {
         this.dataList.clear()
@@ -45,9 +84,10 @@ class PlayAllQuestionGroupAdapter(val context: Context, val dataList: MutableLis
             itv.tv_correctPercent.text = question.correctPercent.toString()
             itv.setOnClickListener {
 
-                val action = PlayingAllQuestionFragmentDirections.actionPlayAllQuestionFragmentToPlayingFragment(
-                    questionId =question.id
-                )
+                val action =
+                    PlayingAllQuestionFragmentDirections.actionPlayAllQuestionFragmentToPlayingFragment(
+                        questionId = question.id
+                    )
 
                 it.findNavController()
                     .navigate(action)
--- app/src/main/java/com/example/qayourself/ui/Fragment/UI_Play_Page/PlayingAllQuestionFragment.kt
+++ app/src/main/java/com/example/qayourself/ui/Fragment/UI_Play_Page/PlayingAllQuestionFragment.kt
@@ -6,7 +6,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import android.widget.AdapterView
+import android.widget.SearchView
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.qayourself.R
@@ -25,7 +27,7 @@ class PlayingAllQuestionFragment : Fragment() {
     }
 
     private lateinit var viewModel: AllQuestionGroupViewModel
-    lateinit var dataList:List<Question>
+    lateinit var dataList: List<Question>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -55,7 +57,20 @@ class PlayingAllQuestionFragment : Fragment() {
             adapter.updateQuestions(dataList)
         })
 
-        spinner_sort.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+        searchView.imeOptions = EditorInfo.IME_ACTION_DONE
+
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return false
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                adapter.filter.filter(newText)
+                return false
+            }
+        })
+
+        spinner_sort.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
             override fun onNothingSelected(parent: AdapterView<*>?) {
 
             }
@@ -66,36 +81,36 @@ class PlayingAllQuestionFragment : Fragment() {
                 position: Int,
                 id: Long
             ) {
-                when(position){
-                    0->{
+                when (position) {
+                    0 -> {
                         dataList = sortAllQuestionByIdAsc(dataList)
                         adapter.updateQuestions(dataList)
                     }
-                    1->{
+                    1 -> {
                         dataList = sortAllQuestionByIdDesc(dataList)
                         adapter.updateQuestions(dataList)
                     }
-                    2->{
+                    2 -> {
                         dataList = sortAllQuestionByNameDesc(dataList)
                         adapter.updateQuestions(dataList)
                     }
-                    3->{
+                    3 -> {
                         dataList = sortAllQuestionByNameAsc(dataList)
                         adapter.updateQuestions(dataList)
                     }
-                    4->{
+                    4 -> {
                         dataList = sortAllQuestionByViewAsc(dataList)
                         adapter.updateQuestions(dataList)
                     }
-                    5->{
+                    5 -> {
                         dataList = sortAllQuestionByViewDesc(dataList)
                         adapter.updateQuestions(dataList)
                     }
-                    6->{
+                    6 -> {
                         dataList = sortAllQuestionByCorrectPercentAsc(dataList)
                         adapter.updateQuestions(dataList)
                     }
-                    7->{
+                    7 -> {
                         dataList = sortAllQuestionByCorrectPercentDesc(dataList)
                         adapter.updateQuestions(dataList)
                     }
@@ -137,6 +152,7 @@ class PlayingAllQuestionFragment : Fragment() {
         })
         return dataList
     }
+
     fun sortAllQuestionByIdDesc(dataList: List<Question>): List<Question> {
         Collections.sort(dataList, object : Comparator<Question> {
             override fun compare(o1: Question?, o2: Question?): Int {
@@ -150,6 +166,7 @@ class PlayingAllQuestionFragment : Fragment() {
         })
         return dataList
     }
+
     fun sortAllQuestionByNameAsc(dataList: List<Question>): List<Question> {
         Collections.sort(dataList, object : Comparator<Question> {
             override fun compare(o1: Question?, o2: Question?): Int {
@@ -163,6 +180,7 @@ class PlayingAllQuestionFragment : Fragment() {
         })
         return dataList
     }
+
     fun sortAllQuestionByNameDesc(dataList: List<Question>): List<Question> {
         Collections.sort(dataList, object : Comparator<Question> {
             override fun compare(o1: Question?, o2: Question?): Int {
@@ -190,6 +208,7 @@ class PlayingAllQuestionFragment : Fragment() {
         })
         return dataList
     }
+
     fun sortAllQuestionByViewDesc(dataList: List<Question>): List<Question> {
         Collections.sort(dataList, object : Comparator<Question> {
             override fun compare(o1: Question?, o2: Question?): Int {
@@ -203,6 +222,7 @@ class PlayingAllQuestionFragment : Fragment() {
         })
         return dataList
     }
+
     fun sortAllQuestionByCorrectPercentAsc(dataList: List<Question>): List<Question> {
         Collections.sort(dataList, object : Comparator<Question> {
             override fun compare(o1: Question?, o2: Question?): Int {
@@ -216,6 +236,7 @@ class PlayingAllQuestionFragment : Fragment() {
         })
         return dataList
     }
+
     fun sortAllQuestionByCorrectPercentDesc(dataList: List<Question>): List<Question> {
         Collections.sort(dataList, object : Comparator<Question> {
             override fun compare(o1: Question?, o2: Question?): Int {
